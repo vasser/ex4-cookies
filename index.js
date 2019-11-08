4901b2e65786d74193ba587ffa965f64be22c32e
svasilkov <svasilkov@quadanalytix.com>
2019-11-09 01:05:22 +0200

    Added Passwords; v1.2.0

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const Cookies = require("./lib/cookies");
 const Email = require("./lib/email");
 const Hash = require("./lib/hash");
+const Passwords = require("./lib/passwords");
 
-module.exports = { Cookies, Email, Hash };
+module.exports = { Cookies, Email, Hash, Passwords };
